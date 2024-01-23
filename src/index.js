81d645db97a250bf98f01b508bc84774025a856e
Sejal003 <123571005+Sejal003@users.noreply.github.com>
2024-01-23 08:10:28 -0800

    Added different functionalities to the app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <Auth0Provider
+    domain="dev-r7qgruisddzgs3oy.us.auth0.com"
+    clientId="05dW8S1Lu7hLYoOrodPZBt4wpM1pIZ4N"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>,
   </React.StrictMode>
 );
 
